be6ad7f2dd93d1d023e421a72172b951afda0597
nghiaduong <duongnghia2904204@gmail.com>
2025-08-14 16:06:22 +0700

    security config

--- Movie_Ticket_Booking/src/main/java/com/org/Movie_Ticket_Booking/config/SecurityConfig.java
+++ Movie_Ticket_Booking/src/main/java/com/org/Movie_Ticket_Booking/config/SecurityConfig.java
@@ -1,4 +1,55 @@
 package com.org.Movie_Ticket_Booking.config;
 
+import com.org.Movie_Ticket_Booking.exception.AppException;
+import com.org.Movie_Ticket_Booking.exception.ErrorCode;
+import com.org.Movie_Ticket_Booking.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> userRepository.findByUsername(username)
+                .map(user -> User.withUsername(user.getName())
+                        .password(user.getPassword())
+                        .authorities(user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getName())).toList())
+                        .build())
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/movies/**", "/auth/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/manager/**").hasRole("CINEMA_MANAGER")
+                        .requestMatchers("/customer/**").hasRole("CUSTOMER")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(
+                        form->form.loginPage("/login").loginProcessingUrl("/authenticateTheUser").permitAll().defaultSuccessUrl("/dashboard", true)
+                ).logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/dashboard")
+                        .permitAll()
+                );
+        return http.build();
+    }
+
 }
---
+++ Movie_Ticket_Booking/src/main/java/com/org/Movie_Ticket_Booking/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.org.Movie_Ticket_Booking.repository;
+
+import com.org.Movie_Ticket_Booking.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByUsername(String username);
+}
