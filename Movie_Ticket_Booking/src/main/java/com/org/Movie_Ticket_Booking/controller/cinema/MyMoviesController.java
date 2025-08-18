c17d284b890c42c3dfe97ec836e63fecb76c749e
Thanh Nam <thanhnamvu584@gmail.com>
2025-08-18 14:15:37 +0700

    task Thiết kết layout Cinema_manager

---
+++ Movie_Ticket_Booking/src/main/java/com/org/Movie_Ticket_Booking/controller/cinema/MyMoviesController.java
@@ -0,0 +1,18 @@
+package com.org.Movie_Ticket_Booking.controller.cinema;
+
+import com.org.Movie_Ticket_Booking.constants.ViewNames;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyMoviesController extends CinemaController {
+
+    // Phim của rạp (quản lý suất chiếu, phòng chiếu)
+    @GetMapping("/my-movies")
+    public String myMovies(Model model) {
+        model.addAttribute("activePage", "my_movies");
+        model.addAttribute("content", ViewNames.CINEMA_MY_MOVIES);
+        return ViewNames.LAYOUT_CINEMA;
+    }
+}
---
+++ Movie_Ticket_Booking/src/main/java/com/org/Movie_Ticket_Booking/controller/cinema/StatisticsCinemaController.java
@@ -0,0 +1,17 @@
+package com.org.Movie_Ticket_Booking.controller.cinema;
+
+import com.org.Movie_Ticket_Booking.constants.ViewNames;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StatisticsCinemaController extends CinemaController {
+
+    @GetMapping("/statistics")
+    public String statistics(Model model) {
+        model.addAttribute("activePage", "statistics");
+        model.addAttribute("content", ViewNames.CINEMA_STATISTICS);
+        return ViewNames.LAYOUT_CINEMA;
+    }
+}
