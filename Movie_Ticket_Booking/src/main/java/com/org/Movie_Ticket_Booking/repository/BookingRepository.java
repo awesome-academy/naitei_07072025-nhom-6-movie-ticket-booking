463ce236f260869fc7dea2e0e0394ef4ab43a9a5
nghiaduong <duongnghia2904204@gmail.com>
2025-08-25 11:49:57 +0700

    booking history

--- Movie_Ticket_Booking/src/main/java/com/org/Movie_Ticket_Booking/repository/BookingRepository.java
+++ Movie_Ticket_Booking/src/main/java/com/org/Movie_Ticket_Booking/repository/BookingRepository.java
@@ -3,6 +3,9 @@ package com.org.Movie_Ticket_Booking.repository;
 import com.org.Movie_Ticket_Booking.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
+    List<Booking> findByUserId(Long id);
 }
